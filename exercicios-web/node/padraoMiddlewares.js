0b701f9d2652a92bd32661eb137574ef0da1312e
Vitória Moura <mouraanav@gmail.com>
2020-12-28 13:52:27 -0300

    Using Midldleware Pattern

--- exercicios-web/node/padraoMiddlewares.js
+++ exercicios-web/node/padraoMiddlewares.js
@@ -1,6 +1,6 @@
 // middleware pattern (chain of responsibility)
 
-// contexto = objeto único
+// contexto: objeto único que é passado para os próximos passos
 const passo1 = (contexto, next) => {
     contexto.valor1 = 'mid1'
     next()
@@ -23,9 +23,9 @@ const exec = (contexto, ...middlewares) => {
 }
 
 const contexto = {}
-// exec(contexto, passo1, passo2, passo3)
+exec(contexto, passo1, passo2, passo3)
 // exec(contexto, passo2, passo1, passo3)
 // exec(contexto, passo1, passo2)
-exec(contexto, passo3, passo2, passo1)
+// exec(contexto, passo3, passo2, passo1)
 
 console.log(contexto)
