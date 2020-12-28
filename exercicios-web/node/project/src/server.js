14309e304cf7d8b4954223848595893372d3accc
Vitória Moura <mouraanav@gmail.com>
2020-12-28 18:36:06 -0300

    Local dataBase with Express

--- exercicios-web/node/project/src/server.js
+++ exercicios-web/node/project/src/server.js
@@ -2,8 +2,12 @@ const port = 3003;
 
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
+const dataBase = require('./dataBase')
 
-// app.get("/produtos", (req, res, next) => {
+app.use(bodyParser.urlencoded({extended: true})) // (O body estando no padrão urlenconded) irá retornar um middleware que fará um parse e tranformara os dados em objetos para serem manipulados no lado do servidor
+
+// app.get("/products", (req, res, next) => {
 //   console.log("Middleware 1...");
 //   next();
 // });
@@ -11,13 +15,41 @@ const app = express();
 
 // O método http "use" é uma outra forma de colocar um middleware na requisição dentro do express, atende qualquer requisição
 // app.use((req, res, next) => {
-//     res.send({ nome: "Notebook", preco: 2567.99 }); // Converter para JSON
+//     res.send({ name: "Notebook", price: 2567.99 }); // Converter para JSON
 // });
 
-app.get("/produtos", (req, res, next) => {
-  res.send({ nome: "Notebook", preco: 2567.99 }); // Converter para JSON
+//Retornar a lista de products
+app.get("/products", (req, res, next) => {
+  res.send(dataBase.getProducts()); // Converter para JSON
 });
 
+app.get("/products/:id", (req, res, next) => {
+  res.send(dataBase.getProduct(req.params.id)); // Dentro dos params da req eu encontro o atributo id
+});
+
+app.post("/products", (req, res, next) => {
+  const product = dataBase.saveProduct({
+      name: req.body.name,
+      price: req.body.price
+  })
+  res.send(product)
+});
+
+app.put("/products/:id", (req, res, next) => {
+    const product = dataBase.saveProduct({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    })
+    res.send(product)
+  });
+
+app.delete("/products/:id", (req, res, next) => {
+  const product = (dataBase.deleteProduct(req.params.id)); // Dentro dos params da req eu encontro o atributo id
+  res.send(product)
+});
+
+
 app.listen(port, () => {
-  console.log(`Executando na porta ${port}`);
+  console.log(`Running at the port ${port}`);
 });
