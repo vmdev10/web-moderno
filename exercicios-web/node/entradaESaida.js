13af7436bd519e48c67691cb923ccff621d0d302
Vitória Moura <mouraanav@gmail.com>
2020-12-29 14:58:18 -0300

    Entrada e saída de dados padrão pelo terminal

--- exercicios-web/node/entradaESaida.js
+++ exercicios-web/node/entradaESaida.js
@@ -1,18 +1,18 @@
-const { exit } = require("process")
+const { exit } = require("process");
 
 // Entrada e saída padrão de dados no terminal, a partir do objeto process
-const anonimo = process.argv.indexOf('-a') !== -1 // Se estiver presente dentro da lista de argumentos o elemento especificado ('-a'), retorna true
-console.log(anonimo)
+const anonimo = process.argv.indexOf("-a") !== -1; // Se estiver presente dentro da lista de argumentos o elemento especificado ('-a'), retorna true
+console.log(anonimo);
 
-if(anonimo) {
-    process.stdout.write('Fala anônimo!\n')
-    process.exit()
+if (anonimo) {
+  process.stdout.write("Fala anônimo!\n");
+  process.exit();
 } else {
-    process.stdout.write('Informe o seu nome: ')
-    process.stdin.on('data', data => {
-        const nome = data.toString().replace('\r\n', '')
+  process.stdout.write("Informe o seu nome: ");
+  process.stdin.on("data", (data) => {
+    const nome = data.toString().replace("\r\n", "");
 
-        process.stdout.write(`Fala ${nome}!!\n`)
-        process.exit()
-    })
+    process.stdout.write(`Fala ${nome}!!\n`);
+    process.exit();
+  });
 }
