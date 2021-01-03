ce07943f3accaf5e13adaf62d4a160092af1e39d
Vitória Moura <mouraanav@gmail.com>
2021-01-03 14:38:38 -0300

    Formulários HTML

---
+++ exercicios-web/html/exercicios/form/server.js
@@ -0,0 +1,18 @@
+const port = 3003
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/usuarios', (req, resp) => {
+    console.log(req.query)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Incluido!</h1>')
+})
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id)
+    console.log(req.body)
+    resp.send('<h1>Parabéns. Usuário Alterado</h1>')
+})
+app.listen(port, () => console.log('Deu certo'))
